acf7890fb3148dbd44952d17fdfffe18524953b0
madaraR1 <madararudzite535@gmail.com>
2026-04-13 15:59:09 +0300

    savienoju home un register

--- app.py
+++ app.py
@@ -7,13 +7,39 @@ def izveidot_db():
     conn = sqlite3.connect("datubaze.db")
     cursor = conn.cursor()
     cursor.execute("""
-    CREATE TABLE "users"(
-    "id"  INTEGER,
-    "username" TEXT,
-    "password" TEXT,
+    CREATE TABLE IF NOT EXISTS "users"(
+    "id"     INTEGER,
+    "username"  TEXT,
+    "password"  TEXT,
     PRIMARY KEY("id")
     )
     """)
     conn.commit()
     conn.close()
 izveidot_db()
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/registreties", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        #print(username)
+        #print(password) 
+  
+        conn = sqlite3.connect("datubaze.db")
+        cursor = conn.cursor()
+        cursor.execute("""INSERT INTO users (username, password) VALUES (?,?) """,(username, password))
+        conn.commit
+        conn.close
+
+        return redirect("/login")
+    
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
