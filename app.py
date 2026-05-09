e86f84d4313a4343a55df239d9d4322e071f0aaa
madaraR1 <madararudzite535@gmail.com>
2026-05-10 02:36:35 +0300

    izveidju filmas tabuku

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "atslega"
 
 def izveidot_db():
     conn = sqlite3.connect("datubaze.db")
@@ -33,8 +34,8 @@ def register():
         conn = sqlite3.connect("datubaze.db")
         cursor = conn.cursor()
         cursor.execute("""INSERT INTO users (username, password) VALUES (?,?) """,(username, password))
-        conn.commit
-        conn.close
+        conn.commit()
+        conn.close()
 
         return redirect("/login")
     
@@ -53,11 +54,76 @@ def login():
         conn.close()
 
         if user:
-            return redirect("/Sveicināti!")
+            session["user_id"] = user[0]
+            session["username"] = user[1]
+            return redirect("/filmas")
         else:
             return "Nepareizs lietotājvārds vai parole"
     
     return render_template("login.html")
 
+def filmas_db():
+
+    conn = sqlite3.connect("filmas.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS filmas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nosaukums TEXT,
+        zanrs TEXT,
+        gads INTEGER
+        user_id INTEGER
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+filmas_db()
+
+@app.route("/filmas", methods=["GET", "POST"])
+def filmas():
+
+    conn = sqlite3.connect("filmas.db")
+    cursor = conn.cursor()
+    if "user_id" not in session:
+        return redirect("/login")
+
+    conn = sqlite3.connect("filmas.db")
+    cursor = conn.cursor()
+
+    user_id = session["user_id"]
+
+
+    if request.method == "POST":
+
+        nosaukums = request.form["nosaukums"]
+        zanrs = request.form["zanrs"]
+        gads = request.form["gads"]
+
+        cursor.execute("""
+        INSERT INTO filmas
+        (nosaukums, zanrs, gads, user_id)
+        VALUES (?, ?, ?, ?)
+        """, (nosaukums, zanrs, gads, user_id))
+
+
+        conn.commit()
+
+    cursor.execute("SELECT * FROM filmas")
+
+    filmas = cursor.fetchall()
+    conn.commit()
+    conn.close()
+
+    return render_template("filmas.html", filmas=filmas)
+
+
+
+        
+
+
 if __name__ == "__main__":
+    izveidot_db()
     app.run(debug=True)
